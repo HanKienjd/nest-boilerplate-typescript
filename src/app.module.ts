cb07e8cbe79c299f6d71234bf5f8eeddabfc1f49
Han Kiên <kienp544@gmail.com>
2024-04-10 15:23:46 +0700

    first update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
+import { ProjectModule } from './modules/project/project.module';
+import { PrismaModule } from 'nestjs-prisma';
+import { ProjectController } from './modules/project/project.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+    }),
+    PrismaModule,
+    ProjectModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
